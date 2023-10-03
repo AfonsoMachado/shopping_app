f040095e7c9bf9bda19f8697c94b38585080a2f5
Afonso Machado <afonsosmachado@gmail.com>
2023-10-03 00:04:56 -0300

    feat: refactor product grid

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,34 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shopping_app/components/product_item.dart';
-import 'package:shopping_app/models/product.dart';
-import 'package:shopping_app/models/product_list.dart';
+import 'package:shopping_app/components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-        ),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
