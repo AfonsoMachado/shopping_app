718862b1376cd9a7c49fa9926245d199f2b647de
Afonso Machado <afonsosmachado@gmail.com>
2023-10-04 14:54:36 -0300

    feat: navigate to cart screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_app/models/cart.dart';
 import 'package:shopping_app/models/product_list.dart';
+import 'package:shopping_app/pages/cart_page.dart';
 import 'package:shopping_app/pages/product_detail.dart';
 import 'package:shopping_app/pages/products_overview_page.dart';
 import 'package:shopping_app/utils/app_routes.dart';
@@ -35,7 +36,10 @@ class MyApp extends StatelessWidget {
         ),
         home: const ProductsOverviewPage(),
         debugShowCheckedModeBanner: false,
-        routes: {AppRoutes.productDetail: (ctx) => const ProductDetailPage()},
+        routes: {
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+          AppRoutes.cart: (ctx) => const CartPage(),
+        },
       ),
     );
   }
