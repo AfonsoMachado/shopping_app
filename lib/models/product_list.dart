373d862a7ce850b99a7a5352193ee989e0ca2087
Afonso Machado <afonsosmachado@gmail.com>
2023-10-05 15:20:47 -0300

    feat: fix clear products list

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -21,6 +21,8 @@ class ProductList with ChangeNotifier {
   }
 
   Future<void> loadProducts() async {
+    _items.clear();
+
     final response = await http.get(Uri.parse(_baseUrl));
     if (response.body == 'null') return;
 
