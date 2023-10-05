001ed50a7d22249856c6b7e60d01b60c5f076188
Afonso Machado <afonsosmachado@gmail.com>
2023-10-05 16:01:36 -0300

    feat: favorite

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -7,6 +11,8 @@ class Product with ChangeNotifier {
   final double price;
   final String imageUrl;
   bool isFavorite;
+
+  final String _baseUrl = "${dotenv.env['FIREBASE_URL']}/products";
 
   Product({
     required this.id,
@@ -17,9 +23,25 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavorite() {
+  void _toggleFavorite() {
     isFavorite = !isFavorite;
     // Notifica todos os change notifiers
     notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    try {
+      _toggleFavorite();
+      final response = await http.patch(
+        Uri.parse('$_baseUrl/$id.json'),
+        body: jsonEncode({"isFavorite": isFavorite}),
+      );
+
+      if (response.statusCode >= 400) {
+        _toggleFavorite();
+      }
+    } catch (_) {
+      _toggleFavorite();
+    }
   }
 }
